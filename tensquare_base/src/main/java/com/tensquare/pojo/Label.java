1eb51cea7eeb14d84a9909cd76faf9443550a658
bingo <2545231110@qq.com>
2019-05-25 02:31:53 +0800

    base编辑

---
+++ tensquare_base/src/main/java/com/tensquare/pojo/Label.java
@@ -0,0 +1,12 @@
+package com.tensquare.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by bingo on 2019/5/25
+ */
+@Entity
+@Table()
+public class Label {
+}
